19abbb8bc72bb2027ce1012469484da6d23693c4
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 14:25:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+  return array
+}
